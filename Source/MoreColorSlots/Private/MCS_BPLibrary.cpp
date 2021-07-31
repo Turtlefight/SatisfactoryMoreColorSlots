ee81e88eece962194c08e431b722de005b5fb2ed
Sven Fisch <turtlefight@ymail.com>
2021-07-31 18:40:26 +0200

    Added option the remove the additional color slots again

--- Source/MoreColorSlots/Private/MCS_BPLibrary.cpp
+++ Source/MoreColorSlots/Private/MCS_BPLibrary.cpp
@@ -1,7 +1,25 @@
 
 #include "MCS_BPLibrary.h"
 
+#include "FGColorInterface.h"
+#include "FGBuildableSubsystem.h"
+#include "Kismet/GameplayStatics.h"
+
 
 void UMCS_BPLibrary::ResetColorSlotsToVanilla(UObject* WorldContextObject) {
+	TArray<AActor*> coloredStuff;
+	UGameplayStatics::GetAllActorsWithInterface(WorldContextObject, UFGColorInterface::StaticClass(), coloredStuff);
 
+	for (AActor* actor : coloredStuff) {
+		if (IFGColorInterface::Execute_GetColorSlot(actor) >= BUILDABLE_COLORS_MAX_SLOTS) {
+			IFGColorInterface::Execute_SetColorSlot(actor, 0);
+		}
+	}
+
+	AFGBuildableSubsystem* buildSystem = AFGBuildableSubsystem::Get(WorldContextObject);
+	if (buildSystem) {
+		buildSystem->mColorSlotsPrimary_Linear.SetNum(BUILDABLE_COLORS_MAX_SLOTS, true);
+		buildSystem->mColorSlotsSecondary_Linear.SetNum(BUILDABLE_COLORS_MAX_SLOTS, true);
+		buildSystem->mNbPlayerExposedSlots = 16;
+	}
 }
